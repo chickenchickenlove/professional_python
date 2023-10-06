5d50666b5833ed4931129a4a1776a03680d77157
ChickenchickenLove <ojt90902@naver.com>
2023-10-06 22:41:12 +0900

    chapter7 study add

--- chapter7/7-10/parameter_decorator.py
+++ chapter7/7-10/parameter_decorator.py
@@ -1,5 +1,23 @@
 import functools
 
+"""
+데코레이터 팩토리를 이용해 매개변수화 된 데코레이터를 생성하는 과정은 다음과 같이 이해하자.
+
+@decorator_factory()
+def f1():
+    ...
+
+-> decorator_factory()는 decorator를 반환하니까.
+@decorator
+def f1():
+    ...
+
+그리고 데코레이터 팩토리의 매개변수들은 각 함수 객체의 클로저를 통해서 전달된다.
+func.__code__.co_freevars를 통해서 확인할 수 있음. 
+
+"""
+
+
 registry = []
 
 
